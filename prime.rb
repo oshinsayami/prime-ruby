063ae1ad5c4b6421c0c39cb41f295c84749d75df
Simran <mdrsimran@gmail.com>
2020-10-26 12:30:46 -0500

    commit tree 5caf1819038ee414a080706b072247199111f20b

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(number)
+    return false if !number.integer?
+
+    return false if number < 2
+
+    return true if number == 2
+
+    (2..number-1).each {|n| return false if number % n == 0}
+
+    true
+
+end
